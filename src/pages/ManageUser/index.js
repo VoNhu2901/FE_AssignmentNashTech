a361344aae515a39f5b3b0c7cbf3e87d07b047ee
tanhao111 <haonguyentan2001@gmail.com>
2022-07-21 13:07:45 +0700

    done Admin Manage page

---
+++ src/pages/ManageUser/index.js
@@ -0,0 +1,642 @@
+import React, { useState, useEffect } from "react";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import SearchIcon from "@mui/icons-material/Search";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import EditIcon from "@mui/icons-material/Edit";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import CloseIcon from "@mui/icons-material/Close";
+
+import "./style.scss";
+
+const data = [
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1902",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1903",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Admin",
+  },
+  {
+    id: "SD1904",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Admin",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Admin",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Admin",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Admin",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Admin",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Admin",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Admin",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+  {
+    id: "SD1901",
+    fullname: "An Nguyen Thuy",
+    username: "annt",
+    joinedDate: "20/06/2019",
+    type: "Staff",
+  },
+];
+
+const ManageUser = () => {
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const [userList, setUserList] = useState([]);
+  const [filterBy, setFilterBy] = useState("All");
+  const [numPage, setNumPage] = useState(0);
+  const [currentCol, setCurrentCol] = useState("");
+  const userPerPage = 20;
+
+  useEffect(() => {
+    // todo: get data from backend
+
+    let len = data.length;
+    let _numPage = Math.ceil(len / 20);
+    setNumPage(_numPage);
+
+    const _data = data.slice((page - 1) * userPerPage, page * userPerPage);
+    setUserList(_data);
+  }, [page]);
+
+  useEffect(() => {
+    if (filterBy === "All") {
+      const _data = data.slice((page - 1) * userPerPage, page * userPerPage);
+      setUserList(_data);
+    } else {
+      const _data = data.filter((user) => user.type === filterBy);
+      const result = _data.slice((page - 1) * userPerPage, page * userPerPage);
+
+      let len = _data.length;
+      let _numPage = Math.ceil(len / 20);
+      console.log(_numPage, len);
+      setNumPage(_numPage);
+
+      setUserList(result);
+    }
+  }, [filterBy, page]);
+
+  const searchUserHandle = () => {
+    // search function here
+    alert(search);
+  };
+
+  const sortByCol = (sortBy) => {};
+
+  const getPaging = () => {
+    const handleNext = () => {
+      let temp = page + 1;
+      if (temp <= numPage) {
+        setPage(temp);
+      }
+    };
+
+    const handlePre = () => {
+      let temp = page - 1;
+      if (temp >= 1) {
+        setPage(temp);
+      }
+    };
+
+    return (
+      <div className="paging text-end">
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          onClick={handlePre}
+        >
+          Previous
+        </button>
+        {Array.from({ length: numPage }, (_, i) => (
+          <button
+            type="button"
+            onClick={(e) => setPage(i + 1)}
+            className={
+              page === i + 1 ? "btn btn-danger" : "btn btn-outline-danger"
+            }
+          >
+            {i + 1}
+          </button>
+        ))}
+        <button
+          type="button"
+          className="btn btn-outline-danger"
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    );
+  };
+
+  // handle delete user here
+  const deleteUser = (code) => {
+    alert(code);
+  };
+
+  // handle edit user here
+  const editUser = (code) => {
+    alert(code);
+  };
+
+  return (
+    <div className="user-list">
+      <div className="title">User List</div>
+
+      <div className="table-board">
+        <div className="left-board">
+          <div className="filter">
+            <div className="dropdown">
+              <button
+                className="btn btn-outline-secondary dropdown-toggle"
+                type="button"
+                id="dropMenuFilterType"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Type
+                <FilterAltIcon />
+              </button>
+              <ul
+                className="dropdown-menu form-check"
+                aria-labelledby="dropMenuFilterType"
+              >
+                <li>
+                  <div>
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value="All"
+                      id="typeAll"
+                      checked={filterBy === "All"}
+                      onClick={() => setFilterBy("All")}
+                    />
+                    <label className="form-check-label" for="typeAll">
+                      All
+                    </label>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value="Admin"
+                      id="typeAdmin"
+                      checked={filterBy === "Admin"}
+                      onClick={() => setFilterBy("Admin")}
+                    />
+                    <label className="form-check-label" for="typeAdmin">
+                      Admin
+                    </label>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value="Staff"
+                      id="typeStaff"
+                      checked={filterBy === "Staff"}
+                      onClick={() => setFilterBy("Staff")}
+                    />
+                    <label className="form-check-label" for="typeStaff">
+                      Staff
+                    </label>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="right-board">
+          <div className="search">
+            <div className="input">
+              <input
+                type="text"
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+              />
+            </div>
+
+            <div>
+              <button className="btn btn-light" onClick={searchUserHandle}>
+                <SearchIcon />
+              </button>
+            </div>
+          </div>
+          <div className="button">
+            <button type="button" className="btn btn-danger">
+              Create new user
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="table-user-list">
+        <table>
+          <thead>
+            <tr>
+              <th className="border-bottom border-3">
+                Staff Code{" "}
+                <button
+                  className="btn btn-outline-light"
+                  onClick={(e) => sortByCol("code")}
+                >
+                  <ArrowDropDownIcon />
+                </button>
+              </th>
+              <th className="border-bottom border-3">
+                Full Name{" "}
+                <button
+                  className="btn btn-outline-light"
+                  onClick={(e) => sortByCol("name")}
+                >
+                  <ArrowDropDownIcon />
+                </button>
+              </th>
+              <th className="border-bottom border-3">Username</th>
+              <th className="border-bottom border-3">
+                Joined Date{" "}
+                <button
+                  className="btn btn-outline-light"
+                  onClick={(e) => sortByCol("date")}
+                >
+                  <ArrowDropDownIcon />
+                </button>
+              </th>
+              <th className="border-bottom border-3">
+                Type{" "}
+                <button
+                  className="btn btn-outline-light"
+                  onClick={(e) => sortByCol("type")}
+                >
+                  <ArrowDropDownIcon />
+                </button>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {userList.map((ele) => (
+              <>
+                <tr
+                  data-bs-toggle="modal"
+                  data-bs-target={"#detailUserViewModal" + ele.id}
+                >
+                  <td className="border-bottom">{ele.id}</td>
+                  <td className="border-bottom">{ele.fullname}</td>
+                  <td className="border-bottom">{ele.username}</td>
+                  <td className="border-bottom">{ele.joinedDate}</td>
+                  <td className="border-bottom">{ele.type}</td>
+                  <td>
+                    <button className="btn btn-outline-secondary border-0">
+                      <EditIcon onClick={(e) => editUser(ele.id)} />
+                    </button>
+                    <button
+                      className="btn btn-outline-danger border-0"
+                      onClick={(e) => deleteUser(ele.id)}
+                    >
+                      <HighlightOffIcon />
+                    </button>{" "}
+                  </td>
+                </tr>
+
+                <div
+                  className="modal fade"
+                  id={"detailUserViewModal" + ele.id}
+                  tabindex="-1"
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true"
+                >
+                  <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h5
+                          className="modal-title text-danger"
+                          id="exampleModalLabel"
+                        >
+                          Detailed User Information
+                        </h5>
+                        <button
+                          type="button"
+                          className="btn btn-outline-danger border-4"
+                          data-bs-dismiss="modal"
+                        >
+                          <CloseIcon />
+                        </button>
+                      </div>
+                      <div className="modal-body">
+                        <div className="detail">
+                          <div className="detail-item">
+                            <div className="label">Staff Code</div>
+                            <div className="value">{ele.id}</div>
+                          </div>
+                          <div className="detail-item">
+                            <div className="label">Full Name</div>
+                            <div className="value">{ele.fullname}</div>
+                          </div>
+                          <div className="detail-item">
+                            <div className="label">Username</div>
+                            <div className="value">{ele.username}</div>
+                          </div>
+                          <div className="detail-item">
+                            <div className="label">Date Of Birth</div>
+                            {/* TODO: change field below */}
+                            <div className="value">{"20/4/2001"}</div>
+                          </div>
+                          <div className="detail-item">
+                            <div className="label">Gender</div>
+                            {/* TODO: change field below */}
+                            <div className="value">{"Male"}</div>
+                          </div>
+                          <div className="detail-item">
+                            <div className="label">Joined Date</div>
+                            <div className="value">{ele.joinedDate}</div>
+                          </div>
+                          <div className="detail-item">
+                            <div className="label">Type</div>
+                            <div className="value">{ele.type}</div>
+                          </div>
+                          <div className="detail-item">
+                            <div className="label">Location</div>
+                            {/* TODO: change field below */}
+                            <div className="value">{"HCM"}</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="paging">{userList && getPaging()}</div>
+    </div>
+  );
+};
+
+export default ManageUser;
