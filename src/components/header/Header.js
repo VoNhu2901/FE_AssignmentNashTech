6f6cb8d04e4bfc1a77294b6d11e89cf3678085f7
tanhao111 <haonguyentan2001@gmail.com>
2022-08-01 15:05:16 +0700

    fix modal

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -14,51 +14,61 @@ const Header = ({ header }) => {
   const [newPassword, setNewPassword] = useState("");
   const [togglePasswordOld, setTogglePasswordOld] = useState(false);
   const [togglePasswordNew, setTogglePasswordNew] = useState(false);
-  const [success, setSuccess] = useState(false);
-
+  const [action, setAction] = useState(0);
+  const [error, setError] = useState("");
   useEffect(() => {
     let name = localStorage.getItem("username");
     setUsername(name);
   }, []);
 
-
   const handleLogOut = () => {
-    authService.logout()
+    authService
+      .logout()
       .then(() => {
         localStorage.clear();
-        navigate("/login")
-      }).catch((err) => {
-        console.log(err)
+        navigate("/login");
       })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const handleChangePassword = () => {
     const userId = localStorage.getItem("userId");
     const payload = {
       userId,
       newPassword,
-      oldPassword
+      oldPassword,
     };
-    userService.changePassword(payload)
+    userService
+      .changePassword(payload)
       .then(() => {
-        setSuccess(true);
-      }).catch((err) => {
+        setAction(1);
+        setNewPassword("");
+        setOldPassword("");
+      })
+      .catch((err) => {
         console.log(err);
         if (err.response.data) {
-          toast.error("Old Password Incorrect!");
+          // toast.error("Old Password Incorrect!");
+          setError("Password is incorrect");
         }
-      })
-  }
+      });
+  };
 
+  const handleClose = () => {
+    setAction(0);
+  };
 
   return (
     <>
       {/* modal notify change password success */}
       <div
-        className="modal fade "
+        className={action === 1 ? "modal show d-block" : " d-none"}
         tabIndex="-1"
         role="dialog"
-        id="modalSuccess"
+        id={"modalSuccess"}
+        aria-hidden="true"
       >
         <div className="modal-dialog modal-dialog-centered modal-sm">
           <div className="modal-content">
@@ -66,12 +76,19 @@ const Header = ({ header }) => {
               <h5 className="modal-title text-danger">Change Password</h5>
             </div>
             <div className="modal-body">
-              <div className="text">
-                <p>
-                  Your password has been changed successfully!
-                </p>
+              <div className="text d-flex flex-column">
+                <p>Your password has been changed successfully!</p>
               </div>
-              <button type="button" className="btn btn-primary ms-5" data-bs-dismiss="modal">Close</button>
+              <div className="w-100 text-end">
+                <button
+                  type="button"
+                  className="btn btn-outline-secondary ms-5 "
+                  data-bs-dismiss="modal"
+                  onClick={handleClose}
+                >
+                  Close
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -79,37 +96,95 @@ const Header = ({ header }) => {
 
       <div className="header">
         <div className="header__title">{header}</div>
-        <div className="header__username " id="user_header" >
+        <div className="header__username " id="user_header">
           <div className="btn-group">
-            <button type="button" class="btn btn-error dropdown-toggle dropdown-toggle-split my-auto" id="dropdownMenuReference" data-bs-toggle="dropdown" aria-expanded="false" data-bs-reference="parent">
+            <button
+              type="button"
+              class="btn btn-error dropdown-toggle dropdown-toggle-split my-auto"
+              id="dropdownMenuReference"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              data-bs-reference="parent"
+            >
               <span className="mx-2">{username}</span>
             </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuReference">
-              <li className="dropdown-item" data-bs-toggle="modal" data-bs-target="#logoutModal">Logout</li>
-              <li className="dropdown-item" data-bs-toggle="modal" data-bs-target="#changePasswordModal" id="li-bottom">Change password</li>
+            <ul
+              className="dropdown-menu"
+              aria-labelledby="dropdownMenuReference"
+            >
+              <li
+                className="dropdown-item"
+                data-bs-toggle="modal"
+                data-bs-target="#logoutModal"
+              >
+                Logout
+              </li>
+              <li
+                className="dropdown-item"
+                // data-bs-toggle="modal"
+                // data-bs-target="#changePasswordModal"
+                id="li-bottom"
+                onClick={() => setAction(2)}
+              >
+                Change password
+              </li>
             </ul>
           </div>
 
           {/* <!-- Modal logout --> */}
-          <div className="modal fade" id="logoutModal" tabIndex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+          <div
+            className="modal fade"
+            id="logoutModal"
+            tabIndex="-2"
+            aria-labelledby="ModalLabel"
+            aria-hidden="true"
+          >
             <div className="modal-dialog modal-sm">
               <div className="modal-content">
                 <div className="modal-header">
-                  <h5 className="modal-title" id="ModalLabel">Are you sure?</h5>
+                  <h5 className="modal-title" id="ModalLabel">
+                    Are you sure?
+                  </h5>
                 </div>
                 <div className="modal-footer">
-                  <button type="button" className="btn btn-secondary" id="btnLogout" onClick={handleLogOut} data-bs-dismiss="modal">Logout</button>
-                  <button type="button" className="btn btn-primary" data-bs-dismiss="modal" id="btnCancel">Cancel</button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    id="btnLogout"
+                    onClick={handleLogOut}
+                    data-bs-dismiss="modal"
+                  >
+                    Logout
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    data-bs-dismiss="modal"
+                    id="btnCancel"
+                  >
+                    Cancel
+                  </button>
                 </div>
               </div>
             </div>
           </div>
+
           {/* <!-- Modal change password --> */}
-          <div className="modal fade " id="changePasswordModal" tabIndex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+          <div
+            className={action === 2 ? " modal show d-block" : " modal d-none"}
+            // id="changePasswordModal"
+            tabIndex="-1"
+            role="dialog"
+
+            // aria-labelledby="ModalLabel"
+            // aria-hidden="true"
+          >
             <div className="modal-dialog">
               <div className="modal-content">
                 <div className="modal-header">
-                  <h5 className="modal-title" id="ModalLabel">Change password</h5>
+                  <h5 className="modal-title" id="ModalLabel">
+                    Change password
+                  </h5>
                 </div>
 
                 <div className="modal-body">
@@ -118,24 +193,33 @@ const Header = ({ header }) => {
                       <label htmlFor="old-pass" className="">
                         Old Password
                       </label>
-                      <input
-                        type={togglePasswordOld ? "text" : "password"}
-                        id="old-pass"
-                        value={oldPassword}
-                        onChange={(e) => setOldPassword(e.target.value)}
-                      />
-                      {!togglePasswordOld ? (
-                        <IconEyeClose
-                          className="icon-eye"
-                          onClick={() => setTogglePasswordOld(true)}
-                        ></IconEyeClose>
-                      ) : (
-                        <IconEyeOpen
-                          className="icon-eye"
-                          onClick={() => setTogglePasswordOld(false)}
-                        ></IconEyeOpen>
-                      )}
+                      <div className="d-flex flex-column">
+                        <input
+                          type={togglePasswordOld ? "text" : "password"}
+                          id="old-pass"
+                          value={oldPassword}
+                          className={
+                            error
+                              ? "border border-danger rounded"
+                              : "border rounded"
+                          }
+                          onChange={(e) => setOldPassword(e.target.value)}
+                        />
+                        {!togglePasswordOld ? (
+                          <IconEyeClose
+                            className="icon-eye"
+                            onClick={() => setTogglePasswordOld(true)}
+                          ></IconEyeClose>
+                        ) : (
+                          <IconEyeOpen
+                            className="icon-eye"
+                            onClick={() => setTogglePasswordOld(false)}
+                          ></IconEyeOpen>
+                        )}
+                        {error && <p className="text-danger fs-6">{error}</p>}
+                      </div>
                     </div>
+
                     <div className="new-password  w-100 d-flex justify-content-between mt-3">
                       <label htmlFor="new-pass" className="pe-3">
                         New Password
@@ -143,6 +227,7 @@ const Header = ({ header }) => {
                       <input
                         type={togglePasswordNew ? "text" : "password"}
                         id="new-pass"
+                        className="border rounded"
                         value={newPassword}
                         onChange={(e) => setNewPassword(e.target.value)}
                       />
@@ -159,15 +244,24 @@ const Header = ({ header }) => {
                       )}
                     </div>
                     <br />
-                    <div className="btn-group-footer d-flex justify-content-end" >
-                      <button type="button " className="btn btn-danger"
+                    <div className="btn-group-footer d-flex justify-content-end">
+                      <button
+                        type="button "
+                        className="btn btn-danger"
                         onClick={handleChangePassword}
                         disabled={!(oldPassword && newPassword)}
-                        data-bs-dismiss={(success ? "modal" : " ")}
-                        data-toggle="modal"
-                        data-target={(success ? "#modalSuccess" : "")}
-                      >Save</button>
-                      <button type="button " className="btn btn-primary ms-2" data-bs-dismiss="modal" id="btnCancel">Cancel</button>
+                      >
+                        Save
+                      </button>
+                      <button
+                        type="button "
+                        className="btn btn-primary ms-2"
+                        data-bs-dismiss="modal"
+                        id="btnCancel"
+                        onClick={() => setAction(0)}
+                      >
+                        Cancel
+                      </button>
                     </div>
                   </div>
                 </div>
